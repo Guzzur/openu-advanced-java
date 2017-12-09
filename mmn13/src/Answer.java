4c85050434667f0817d4ba043e96f58d41b58247
Guzzur <frazykov@gmail.com>
2017-12-09 23:38:52 +0200

    Javadoc

--- mmn13/src/Answer.java
+++ mmn13/src/Answer.java
@@ -2,15 +2,27 @@ public class Answer {
     private boolean isRight;
     private String content;
 
+    /**
+     * C'tor
+     * @param isRight status of this answer
+     * @param content the string
+     */
     public Answer(boolean isRight, String content) {
         this.isRight = isRight;
         this.content = new String(content);
     }
 
+    /**
+     * @return true if it's right
+     */
     public boolean getIsRight() {
         return this.isRight;
     }
 
+    /**
+     * Overrides Object's toString()
+     * @return formatted string
+     */
     @Override
     public String toString() {
         return this.content;
--- mmn13/src/Question.java
+++ mmn13/src/Question.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
+/**
+ * Question
+ */
 public class Question {
     private int id;
     private String content;
     private ArrayList<Answer> answers = new ArrayList<>();
 
+    // defines answer status enum
     public static enum answerStatus {
         right,
         wrong,
         unknown;
     }
 
+    // holds question's status
     private answerStatus status;
 
+    /**
+     * C'tor
+     * @param id in the exam
+     * @param buffer to parse the answers from
+     */
     public Question(int id, String[] buffer) {
         this.id = id;
 
@@ -26,10 +35,16 @@ public class Question {
             this.answers.add(new Answer(false, buffer[i]));
     }
 
+    /**
+     * @return the string of the question
+     */
     public String getContent() {
         return this.content;
     }
 
+    /**
+     * @return the array of answers
+     */
     public Answer[] getAnswers() {
         Answer ans[] = new Answer[this.answers.size()];
 
@@ -39,6 +54,10 @@ public class Question {
         return ans;
     }
 
+    /**
+     * Overrides Object's toString()
+     * @return formatted string
+     */
     @Override
     public String toString() {
         String retVal = "";
@@ -50,14 +69,23 @@ public class Question {
         return content + retVal;
     }
 
+    /**
+     * @return status of question
+     */
     public answerStatus getStatus() {
         return this.status;
     }
 
+    /**
+     * @param status of question
+     */
     public void setStatus(answerStatus status) {
         this.status = status;
     }
 
+    /**
+     * Shuffles the question's answers
+     */
     public void shuffleAnswers() {
         Collections.shuffle(this.answers);
     }
