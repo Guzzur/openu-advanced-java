50d539980c90f47836793cd33defc6b7d792ddfb
Guzzur <frazykov@gmail.com>
2017-12-16 02:30:40 +0200

    Finished

--- mmn13/q2/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // creates GUI
-        GraphicUI gui = new GraphicUI("Sudoku");
-        // runs GUI
-        gui.present();
-    }
-}
